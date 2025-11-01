7cd3bff1c0188014b748c6e515846d73264ad40b
ytli <2072394106@qq.com>
2025-11-01 21:02:58 +0800

    feat: 搭架子

---
+++ client/Assets/Scripts/GamePlay/Bullet/BaseBulletEntity.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 子弹基类，4种，近战，远程子弹，投掷，固定点爆破
+/// </summary>
+public class BaseBulletEntity : MonoBehaviour
+{
+
+}
---
+++ client/Assets/Scripts/GamePlay/Player/ThirdPlayerMoveCtrl.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 挂上此脚本，就是第三人称控制器
+/// </summary>
+public class ThirdPlayerMoveCtrl : MonoBehaviour
+{
+ 
+}
