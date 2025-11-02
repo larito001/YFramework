bed7083520623b5c2f5145d15ecd3b782d71418b
larito <2072394106@qq.com>
2025-11-02 23:23:57 +0800

    feat: 普通第一人称控制器完成

--- client/Assets/Scripts/GamePlay/Player/ThirdPlayerMoveCtrl.cs
+++ client/Assets/Scripts/GamePlay/Player/ThirdPlayerMoveCtrl.cs
@@ -25,6 +25,10 @@ public class ThirdPlayerMoveCtrl : MonoBehaviour
     [SerializeField] LayerMask probeMask = -1, //检测的层
         stairsMask = -1; //楼梯的曾
 
+    [SerializeField]
+    Transform playerInputSpace = default;//输入空间，基于什么移动
+    
+    
     Vector3 contactNormal, //当前接触面的法线
         steepNormal; //墙面法线
 
@@ -95,7 +99,21 @@ public class ThirdPlayerMoveCtrl : MonoBehaviour
         desiredJump |= Input.GetButtonDown("Jump"); //desiredJump一直为true，直到desiredJump被手动改为false
         playerInput = Vector2.ClampMagnitude(playerInput, 1f);
         //目标速度
-        desiredVelocity = new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
+        if (playerInputSpace) {
+            //防止太远导致输入太小
+            Vector3 forward = playerInputSpace.forward;
+            forward.y = 0f;
+            forward.Normalize();
+            Vector3 right = playerInputSpace.right;
+            right.y = 0f;
+            right.Normalize();
+            desiredVelocity =(forward * playerInput.y + right * playerInput.x) * maxSpeed;
+        }
+        else
+        {
+            desiredVelocity = new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed; 
+        }
+   
         GetComponent<Renderer>().material.SetColor(
             "_Color", OnGround ? Color.black : Color.white
         );
