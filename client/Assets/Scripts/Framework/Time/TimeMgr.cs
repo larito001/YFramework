936847c10ba442908f24c4335d98c65d240de8c8
larito <2072394106@qq.com>
2025-11-29 13:36:44 +0800

    剔除无用代码

--- client/Assets/Scripts/Framework/Time/TimeMgr.cs
+++ client/Assets/Scripts/Framework/Time/TimeMgr.cs
@@ -1,175 +1,311 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Drawing.Printing;
-using System.Linq;
 using UnityEngine;
-using UnityEngine.Events;
-namespace YOTO
-{
-    public enum TimerType
-    {
-        Time,
-        Frame
-    }
-    public delegate void TimerCallback();
-    public class TimeMgr
-    {
-    
-        private Dictionary<TimerCallback,Timer> timers = new Dictionary<TimerCallback,Timer>();
-        private TimeScaler scaler = new TimeScaler();
-        public void Init()
-        {
 
-        }
-        public void RemoveTimer(TimerCallback callback)
+public delegate void TimerCallback(object param);
+
+public class Timers
+{
+    public static int repeat;
+    public static float time;
+
+    public static bool catchCallbackExceptions = true;
+
+    Dictionary<TimerCallback, Anymous_T> _items;
+    Dictionary<TimerCallback, Anymous_T> _toAdd;
+    List<Anymous_T> _toRemove;
+    List<Anymous_T> _pool;
+
+    TimersEngine _engine;
+    GameObject gameObject;
+
+    private static Timers _inst;
+
+    public static Timers inst
+    {
+        get
         {
-            if (timers.ContainsKey(callback))
-            {
-                timers[callback].Stop();
-                timers.Remove(callback);
-            }
-        }
-        public void DelayCall(TimerCallback callback, float delayTime)
-        {
-            Timer timer = new Timer();
-            timer.Init(callback, TimerType.Time, delayTime, 1);
-            timers.Add(callback,timer);
-        }
-        
-        public void DelayCallFram(TimerCallback callback, int delayFrames)
-        {
-            Timer timer = new Timer();
-            timer.Init(callback, TimerType.Frame, delayFrames, 1);
-            timers.Add(callback,timer);
-        }
-        public void LoopCall(TimerCallback callback, float time,int loopCount=-1)
-        {
-            Timer timer = new Timer();
-            timer.Init(callback, TimerType.Time, time, loopCount);  // -1表示无限循环
-            timers.Add(callback,timer);
-        }
-        public void LoopCallFram(TimerCallback callback, int frames)
-        {
-            Timer timer = new Timer();
-            timer.Init(callback, TimerType.Frame, frames, -1);  // -1表示无限循环
-            timers.Add(callback,timer);
-        }
-        public void SetTimeScale(float scale)
-        {
-            scaler.SetTimeScale(scale);
-        }
-        public void Update(float deltaTime)
-        {
-            var shoot = timers.ToList();
-            foreach (var keyValuePair in shoot)
-            {
-                if (timers.ContainsKey(keyValuePair.Key))
-                {
-                    keyValuePair.Value.Update(deltaTime);
-                    if (timers[keyValuePair.Key].IsStop())
-                    {
-                        timers.Remove(keyValuePair.Key);    
-                    }
-                  
-                }
-         
-            }
-            
+            if (_inst == null)
+                _inst = new Timers();
+            return _inst;
         }
     }
-    class Timer { 
-        private TimerType type;
-        private bool  isStoped;
-        private bool isPause;
-        private int loopCount;
-        private float elapsedTime;
-        private float stepTime;
-        private TimerCallback callback;
-        public void Init(TimerCallback callback, TimerType type, float stepTime,  int loopCount = -1)
+
+    public Timers()
+    {
+        _inst = this;
+        gameObject = new GameObject("[FairyGUI.Timers]");
+        gameObject.hideFlags = HideFlags.HideInHierarchy;
+        gameObject.SetActive(true);
+        Object.DontDestroyOnLoad(gameObject);
+
+        _engine = gameObject.AddComponent<TimersEngine>();
+
+        _items = new Dictionary<TimerCallback, Anymous_T>();
+        _toAdd = new Dictionary<TimerCallback, Anymous_T>();
+        _toRemove = new List<Anymous_T>();
+        _pool = new List<Anymous_T>(100);
+    }
+
+    public void Add(float interval, int repeat, TimerCallback callback, bool isCallImmediately = false)
+    {
+        Add(interval, repeat, callback, null, isCallImmediately);
+    }
+
+    public void Add(float interval, TimerCallback callback)
+    {
+        Add(interval, 1, callback, null);
+    }
+
+    /**
+     * @interval in seconds
+     * @repeat 0 indicate loop infinitely, otherwise the run count
+     **/
+    public void Add(float interval, int repeat, TimerCallback callback, object callbackParam,
+        bool isCallImmediately = false)
+    {
+        if (callback == null)
         {
-            this.type = type;
-            this.loopCount = loopCount;
-            this.stepTime = stepTime;
-            this.callback = callback;
-            isStoped = false;
+            Debug.LogWarning("timer callback is null, " + interval + "," + repeat);
+            return;
         }
-        public bool IsStop()
+
+        if (isCallImmediately)
+            callback(callbackParam);
+        Anymous_T t;
+        if (_items.TryGetValue(callback, out t))
         {
-            return isStoped;
+            t.set(interval, repeat, callback, callbackParam);
+            t.elapsed = 0;
+            t.deleted = false;
+            return;
         }
-        public bool isComplete()
+
+        if (_toAdd.TryGetValue(callback, out t))
         {
-            return loopCount == 0;
+            t.set(interval, repeat, callback, callbackParam);
+            return;
         }
-        public void ChangeStepTime(float stepTime)
+
+        t = GetFromPool();
+        t.interval = interval;
+        t.repeat = repeat;
+        t.callback = callback;
+        t.param = callbackParam;
+        _toAdd[callback] = t;
+    }
+
+    public void CallLater(TimerCallback callback)
+    {
+        Add(0.001f, 1, callback);
+    }
+
+    public void CallLater(TimerCallback callback, object callbackParam)
+    {
+        Add(0.001f, 1, callback, callbackParam);
+    }
+
+    public void AddUpdate(TimerCallback callback)
+    {
+        Add(0.001f, 0, callback);
+    }
+
+    public void AddUpdate(TimerCallback callback, object callbackParam)
+    {
+        Add(0.001f, 0, callback, callbackParam);
+    }
+
+    public void StartCoroutine(IEnumerator routine)
+    {
+        _engine.StartCoroutine(routine);
+    }
+
+    public void StopCoroutine(IEnumerator routine)
+    {
+        _engine.StopCoroutine(routine);
+    }
+
+    public bool Exists(TimerCallback callback)
+    {
+        if (_toAdd.ContainsKey(callback))
+            return true;
+
+        Anymous_T at;
+        if (_items.TryGetValue(callback, out at))
+            return !at.deleted;
+
+        return false;
+    }
+
+    public void Remove(TimerCallback callback)
+    {
+        Anymous_T t;
+        if (_toAdd.TryGetValue(callback, out t))
         {
-            this.stepTime = stepTime;
+            _toAdd.Remove(callback);
+            ReturnToPool(t);
         }
-        public void Pause()
+
+        if (_items.TryGetValue(callback, out t))
+            t.deleted = true;
+    }
+
+    private Anymous_T GetFromPool()
+    {
+        Anymous_T t;
+        int cnt = _pool.Count;
+        if (cnt > 0)
         {
-            isPause = true;
+            t = _pool[cnt - 1];
+            _pool.RemoveAt(cnt - 1);
+            t.deleted = false;
+            t.elapsed = 0;
         }
-        public void Resume()
+        else
+            t = new Anymous_T();
+
+        return t;
+    }
+
+    private void ReturnToPool(Anymous_T t)
+    {
+        t.callback = null;
+        _pool.Add(t);
+    }
+
+    public void Update()
+    {
+        float dt = Time.unscaledDeltaTime;
+        Dictionary<TimerCallback, Anymous_T>.Enumerator iter;
+#if UNITY_PROFILER_TEST
+            UnityEngine.Profiling.Profiler.BeginSample("Timers.Update foreach");
+#endif
+        if (_items.Count > 0)
         {
-            isPause = false;
-        }
-        public void Stop()
-        {
-            isStoped = true;
-        }
-        public void Update(float deltaTime)
-        {
-            if (isPause) return;
-            if(isStoped) return;
-            if (loopCount == 0) return;
-            if (type==TimerType.Time)
+            iter = _items.GetEnumerator();
+            while (iter.MoveNext())
             {
-                elapsedTime += deltaTime;
-                if(elapsedTime>= stepTime)
+                Anymous_T i = iter.Current.Value;
+                if (i.deleted)
                 {
-                    elapsedTime -= stepTime;
-                    if (callback!=null)
+                    _toRemove.Add(i);
+                    continue;
+                }
+
+                i.elapsed += dt;
+                if (i.elapsed < i.interval)
+                    continue;
+
+                i.elapsed -= i.interval;
+                if (i.elapsed < 0 || i.elapsed > 0.03f)
+                    i.elapsed = 0;
+
+                if (i.repeat > 0)
+                {
+                    i.repeat--;
+                    if (i.repeat == 0)
                     {
-                        try {
-                            callback();
-                        } catch(Exception e)
+                        i.deleted = true;
+                        _toRemove.Add(i);
+                    }
+                }
+
+                repeat = i.repeat;
+                if (i.callback != null)
+                {
+                    if (catchCallbackExceptions)
+                    {
+                        try
                         {
-                            Debug.Log("Timerִд:"+e);
+#if UNITY_PROFILER_TEST
+								UnityEngine.Profiling.Profiler.BeginSample($"Timers callback = {i.callback.Method.DeclaringType.FullName}_{i.callback.Method.DeclaringType.Name}");
+#endif
+                            i.callback(i.param);
+#if UNITY_PROFILER_TEST
+								UnityEngine.Profiling.Profiler.EndSample();
+#endif
+                        }
+                        catch (System.Exception e)
+                        {
+                            i.deleted = true;
+                            Debug.LogError("FGui:TimerError(internal=" + i.interval + ", repeat=" + i.repeat +
+                                           ",functionName" + i.callback.Method.Name + ") callback error > " +
+                                           e.StackTrace);
                         }
                     }
-                    loopCount--;
+                    else
+                        i.callback(i.param);
                 }
+            }
 
-            }
-            else if (type == TimerType.Frame)
-            {
-                ++elapsedTime;
-                if (elapsedTime>=stepTime)
-                {
-                    elapsedTime = 0;
-                    try
-                    {
-                        callback();
-                    }
-                    catch (Exception e)
-                    {
-                        Debug.Log("Timerִд:" + e);
-                    }
-                    loopCount--;
-                }
-            }
-            if (loopCount==0)
-            {
-                Stop();
-            }
+            iter.Dispose();
         }
-    }
-    public class TimeScaler
-    {
-        public void SetTimeScale(float scale)
+#if UNITY_PROFILER_TEST
+            UnityEngine.Profiling.Profiler.EndSample();
+#endif
+
+#if UNITY_PROFILER_TEST
+            UnityEngine.Profiling.Profiler.BeginSample("Timers.Update remove");
+#endif
+        int len = _toRemove.Count;
+        if (len > 0)
         {
+            for (int k = 0; k < len; k++)
+            {
+                Anymous_T i = _toRemove[k];
+                if (i.deleted && i.callback != null)
+                {
+                    _items.Remove(i.callback);
+                    ReturnToPool(i);
+                }
+            }
 
+            _toRemove.Clear();
         }
+#if UNITY_PROFILER_TEST
+            UnityEngine.Profiling.Profiler.EndSample();
+#endif
+
+#if UNITY_PROFILER_TEST
+            UnityEngine.Profiling.Profiler.BeginSample("Timers.Update add");
+#endif
+        if (_toAdd.Count > 0)
+        {
+            iter = _toAdd.GetEnumerator();
+            while (iter.MoveNext())
+                _items.Add(iter.Current.Key, iter.Current.Value);
+            iter.Dispose();
+            _toAdd.Clear();
+        }
+#if UNITY_PROFILER_TEST
+            UnityEngine.Profiling.Profiler.EndSample();
+#endif
+    }
+}
+
+class Anymous_T
+{
+    public float interval;
+    public int repeat;
+    public TimerCallback callback;
+    public object param;
+
+    public float elapsed;
+    public bool deleted;
+
+    public void set(float interval, int repeat, TimerCallback callback, object param)
+    {
+        this.interval = interval;
+        this.repeat = repeat;
+        this.callback = callback;
+        this.param = param;
+    }
+}
+
+class TimersEngine : MonoBehaviour
+{
+    void Update()
+    {
+        Timers.inst.Update();
     }
 }
